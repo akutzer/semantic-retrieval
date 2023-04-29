cde331bb542e887d34bf8987113a0ee27bee1434
Aaron Kutzer <aaron.kutzer@mailbox.tu-dresden.de>
2023-04-29 21:42:13 +0200

    added shitty train script for demonstration

---
+++ retrieval/training/train.py
@@ -0,0 +1,59 @@
+import torch
+from tqdm import tqdm
+
+from retrieval.configs import BaseConfig
+from retrieval.data import DataIterator
+from retrieval.models import ColBERT
+
+# tensorboard --logdir=runs
+# http://localhost:6006/
+from torch.utils.tensorboard import SummaryWriter
+
+
+MODEL_PATH = "bert-base-uncased" # "../../data/colbertv2.0/" # "bert-base-uncased"
+DEVICE = "cuda:0"
+
+config = BaseConfig(
+    tok_name_or_path=MODEL_PATH,
+    backbone_name_or_path=MODEL_PATH,
+    epochs = 10,
+    batch_size = 32,
+    accum_steps = 2,
+    similarity="cosine", 
+    intra_batch_similarity=True)
+
+writer = SummaryWriter()
+
+triples_path = "/home/aaron/Documents/Studium/Informatik/6_Semester/KP BigData/semantic-retrieval/data/fandom-qa/harry_potter_qa/triples.train.tsv"
+queries_path = "/home/aaron/Documents/Studium/Informatik/6_Semester/KP BigData/semantic-retrieval/data/fandom-qa/harry_potter_qa/queries.train.tsv"
+passages_path = "/home/aaron/Documents/Studium/Informatik/6_Semester/KP BigData/semantic-retrieval/data/fandom-qa/harry_potter_qa/passages.train.tsv"
+
+data_iter = DataIterator(config, triples_path, queries_path, passages_path)
+
+colbert = ColBERT(config, device=DEVICE)
+
+optimizer = torch.optim.AdamW(colbert.parameters(), lr=5e-6, eps=1e-8)
+criterion = torch.nn.CrossEntropyLoss()
+
+for i, batch in enumerate(tqdm(data_iter)):
+    #B = sum(map(lambda x: x[0].shape[0], batch))
+    optimizer.zero_grad()
+    losses = 0
+    accs = 0
+    for sub_batch in batch:
+        q_tokens, q_masks, p_tokens, p_masks = sub_batch
+        Q, P = (q_tokens, q_masks), (p_tokens, p_masks)
+        sub_B = q_tokens.shape[0]
+
+        out = colbert(Q, P)        
+        accs += torch.sum(out.detach().max(dim=-1).indices == torch.arange(0, sub_B, device=DEVICE, dtype=torch.long))        
+        loss = criterion(out, torch.arange(0, sub_B, device=DEVICE, dtype=torch.long))
+        loss *= 1 / config.batch_size
+        loss.backward()
+
+        losses += loss.item()
+    
+    optimizer.step()   
+
+    writer.add_scalar('Loss/train', losses, i)
+    writer.add_scalar('Acc/train', accs / config.batch_size, i)
