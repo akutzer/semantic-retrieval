129113de548b88b2073998c0671db3343b52a9f3
Aaron Kutzer <aaron.kutzer@mailbox.tu-dresden.de>
2023-04-29 22:07:56 +0200

    lil changes 2 train.py

--- retrieval/training/train.py
+++ retrieval/training/train.py
@@ -1,5 +1,8 @@
-import torch
+import random
 from tqdm import tqdm
+import torch
+import numpy as np
+
 
 from retrieval.configs import BaseConfig
 from retrieval.data import DataIterator
@@ -10,7 +13,13 @@ from retrieval.models import ColBERT
 from torch.utils.tensorboard import SummaryWriter
 
 
-MODEL_PATH = "bert-base-uncased" # "../../data/colbertv2.0/" # "bert-base-uncased"
+random.seed(125)
+np.random.seed(125)
+torch.manual_seed(125)
+torch.cuda.manual_seed_all(125)
+
+
+MODEL_PATH = "../../data/colbertv2.0/" # "bert-base-uncased" #"bert-base-uncased" # 
 DEVICE = "cuda:0"
 
 config = BaseConfig(
@@ -35,25 +44,29 @@ colbert = ColBERT(config, device=DEVICE)
 optimizer = torch.optim.AdamW(colbert.parameters(), lr=5e-6, eps=1e-8)
 criterion = torch.nn.CrossEntropyLoss()
 
-for i, batch in enumerate(tqdm(data_iter)):
-    #B = sum(map(lambda x: x[0].shape[0], batch))
-    optimizer.zero_grad()
-    losses = 0
-    accs = 0
-    for sub_batch in batch:
-        q_tokens, q_masks, p_tokens, p_masks = sub_batch
-        Q, P = (q_tokens, q_masks), (p_tokens, p_masks)
-        sub_B = q_tokens.shape[0]
+for epoch in range(1, config.epochs+1):
+    data_iter.shuffle()
+    for i, batch in enumerate(tqdm(data_iter)):
+        #B = sum(map(lambda x: x[0].shape[0], batch))
+        optimizer.zero_grad()
+        losses = 0
+        accs = 0
+        for sub_batch in batch:
+            q_tokens, q_masks, p_tokens, p_masks = sub_batch
+            Q, P = (q_tokens, q_masks), (p_tokens, p_masks)
+            sub_B = q_tokens.shape[0]
 
-        out = colbert(Q, P)        
-        accs += torch.sum(out.detach().max(dim=-1).indices == torch.arange(0, sub_B, device=DEVICE, dtype=torch.long))        
-        loss = criterion(out, torch.arange(0, sub_B, device=DEVICE, dtype=torch.long))
-        loss *= 1 / config.batch_size
-        loss.backward()
+            out = colbert(Q, P)        
+            accs += torch.sum(out.detach().max(dim=-1).indices == torch.arange(0, sub_B, device=DEVICE, dtype=torch.long))        
+            loss = criterion(out, torch.arange(0, sub_B, device=DEVICE, dtype=torch.long))
+            loss *= 1 / config.batch_size
+            loss.backward()
 
-        losses += loss.item()
-    
-    optimizer.step()   
+            losses += loss.item()
+        
+        optimizer.step()   
 
-    writer.add_scalar('Loss/train', losses, i)
-    writer.add_scalar('Acc/train', accs / config.batch_size, i)
+        writer.add_scalar('Loss/train', losses, epoch*len(data_iter) + i)
+        writer.add_scalar('Acc/train', accs / config.batch_size, epoch*len(data_iter) + i)
+
+    data_iter.reset()
