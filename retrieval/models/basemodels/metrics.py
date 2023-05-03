667126944a2911f0d8be83aa019199b719903d73
till4513 <till4513@gmail.com>
2023-05-03 09:20:40 +0200

    small description change

--- retrieval/models/basemodels/metrics.py
+++ retrieval/models/basemodels/metrics.py
@@ -9,16 +9,28 @@ class Metrics:
         self.qpp_triples = qpp_triples
         self.pqq_triples = pqq_triples
         self.dataset_name = dataset_name
+
+        # best paragraphs for queries
+        self.best_matches_for_query = self.bestMatches(True)
+
+        # best queries for paragraphs
+        # self.best_matches_for_paragraph = self.bestMatches(False)
+
+        # inverse of mapping
         self.pid_row_mapping = {v: k for k, v in row_pid_mapping.items()}
         self.qid_col_mapping = {v: k for k, v in col_qid_mapping.items()}
 
 
-    def __bestMatches__(self, passages=True):
+    # position where element would be if sorted
+    # either columnwise (passages=True) or rowwise (passages=False)
+    def bestMatches(self, passages=True):
         if passages:
             return np.argsort(np.argsort(-self.M, axis=0), axis=0)
         else:
             return np.argsort(np.argsort(-self.M, axis=1), axis=1)
-    
+     
+
+    # for every triple returns position where tf idf ranked it (only position array)
     def positionOfPositive(self, pqq=False):
         pos = []
         if pqq:
@@ -28,13 +40,11 @@ class Metrics:
             row_ind = 1
             col_ind = 0
 
-        
-        best_matches_M = self.__bestMatches__(passages=True)
             
         for triple in self.qpp_triples:
             row = self.pid_row_mapping[triple[row_ind]]
             col = self.qid_col_mapping[triple[col_ind]] 
-            pos.append(best_matches_M[row,col])
+            pos.append(self.best_matches_for_query[row,col])
 
         return pos
     
