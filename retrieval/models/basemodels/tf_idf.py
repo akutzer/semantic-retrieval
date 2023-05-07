658564698129623e3f134816da7d26c3eca6b710
till4513 <till4513@gmail.com>
2023-05-07 11:59:35 +0200

    added support for single question answering, precomputation of passages

--- retrieval/models/basemodels/tf_idf.py
+++ retrieval/models/basemodels/tf_idf.py
@@ -1,73 +1,89 @@
-import pandas as pd
-import os 
 import numpy as np
+import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from retrieval.data.queries import Queries
 from retrieval.data.passages import Passages
 from retrieval.data.triples import Triples
+from retrieval.data.dataset import TripleDataset
 from retrieval.models.basemodels.metrics import Metrics 
+from retrieval.configs import BaseConfig
+
 
 
 
 '''
-#   Evaluates test data by using tf-idf as a base model. Measurement: is one of the best -k- predicted answers the correct one?
-#   Calculates the dot product between every paragraph tf-idf vector and the query tf-idf vector and chooses the largest -k- scores. 
-#   (X@q_vec.T, where X is matrix of all paragraph vectors and q_vec is the query vector)
+#   Calculates the dot product between every paragraph tf-idf vector and the query tf-idf vector and chooses the best . 
+#   (X@Q.T, where X is matrix of all paragraph vectors and Q is the query matrix)
 '''
 
-FOLDERS = ['../../../data/fandom_qa/harry_potter_qa_small'
+FOLDERS = ['../../../data/fandom-qa/harry_potter_qa'
            #'../../../data/fandom_qa/witcher_qa'
            ]
 
 # number of paragraphs that are acceptable for a question
 K = 5
 
-def read_df_from_tsv(file, column_names):
-    return pd.read_csv(file, sep='\t', header=None).rename(columns=dict(enumerate(column_names)))
+# def read_df_from_tsv(file, column_names):
+#     return pd.read_csv(file, sep='\t', header=None).rename(columns=dict(enumerate(column_names)))
+
+class TfIdf():
+    def __init__(self, folders, combine_paragraphs=True, mode="qpp"):
+        self.folders = folders
+        self.passage_files = [x + '/' + y for x in folders for y in os.listdir(x) if 'passages' in y and '.tsv' in y]
+        self.question_files = [x + '/' + y for x in folders for y in os.listdir(x) if 'queries' in y and '.tsv' in y]
+        self.triple_files = [x + '/' + y for x in folders for y in os.listdir(x) if 'triples' in y and '.tsv' in y]
+
+        self.folder_objects = [(Passages(passage_file), Queries(question_file), Triples(triple_file, mode)) for passage_file,question_file, triple_file in zip(self.passage_files, self.question_files, self.triple_files)]
+
+        self.paragraphs = None
+        if combine_paragraphs:
+            self.paragraphs = np.array([passages.values() for passages in list(zip(*self.folder_objects))[0]]).flatten()
+
+        self.vectorizer, self.X = self.tfIDFCreatorFromArr(self.paragraphs)
 
 
-def tfIDFCreatorFromArr(arr, min_df=10):
-    vectorizer = TfidfVectorizer(strip_accents='unicode', min_df=min_df)
-    X = vectorizer.fit_transform(arr)
-    return vectorizer, X
-
-
-def tfIDF(k, FOLDERS):
-    passage_files = [x + '/' + y for x in FOLDERS for y in os.listdir(x) if 'passages' in y and '.tsv' in y]
-    question_files = [x + '/' + y for x in FOLDERS for y in os.listdir(x) if 'queries' in y and '.tsv' in y]
-    triple_files = [x + '/' + y for x in FOLDERS for y in os.listdir(x) if 'triples' in y and '.tsv' in y]
-
-    for i in range(len(FOLDERS)):
-        #p_df = read_df_from_tsv(passage_files[i], ['PID', 'paragraph'])
-        #q_df = read_df_from_tsv(question_files[i], ['QID', 'query'])
-        #t_df = read_df_from_tsv(triple_files[i], ['QID', 'PID+', 'PID-'])
-        
-        # read files into dataframe
-        paragraphs = Passages(passage_files[i])
-        queries = Queries(question_files[i])
-        qpp_triples = Triples(triple_files[i])
-
-        # order is the same i googled it
-        row_pid_mapping = dict(enumerate(paragraphs.keys()))
-        col_qid_mapping = dict(enumerate(queries.keys()))
-
-        # create tf-idf vectorizer and matrix
-        vectorizer, X = tfIDFCreatorFromArr(paragraphs.values())
-
-        Q = vectorizer.transform(queries.values()).T
-        
-        Q = Q.toarray()
-        X = X.toarray()
+    def answerQuestion(self, question):
+        Q = self.vectorizer.transform([question]).T
+        X = self.X.toarray()
         M = X@Q
 
-        metrics = Metrics(M, row_pid_mapping, col_qid_mapping, dataset_name=FOLDERS[i], qpp_triples=qpp_triples)
-        metrics.isInBestK(5)
-        metrics.printStatistics(6,1)
+        max_ind = np.argmax(M)
+        return max_ind, self.paragraphs[max_ind]
 
 
 
-def main():
-    tfIDF(K, FOLDERS)
+
+    def tfIDFCreatorFromArr(self, arr, min_df=10):
+        vectorizer = TfidfVectorizer(strip_accents='unicode', min_df=min_df)
+        X = vectorizer.fit_transform(arr)
+        return vectorizer, X
+    
+    
+    def metrics(self,k):
+        for i in range(len(self.folders)):
+            paragraphs, queries, qpp_triples = self.folder_objects[i]
+
+            # order is the same i googled it
+            row_pid_mapping = dict(enumerate(paragraphs.keys()))
+            col_qid_mapping = dict(enumerate(queries.keys()))
+
+            # create tf-idf vectorizer and matrix
+            vectorizer, X = self.tfIDFCreatorFromArr(paragraphs.values())
+
+            Q = vectorizer.transform(queries.values()).T
+            
+            Q = Q.toarray()
+            X = X.toarray()
+            M = X@Q
+
+            metrics = Metrics(M, row_pid_mapping, col_qid_mapping, dataset_name=self.folders[i], qpp_triples=qpp_triples)
+            metrics.printStatistics(6,1)
+
+
+
+
 
 if __name__ == "__main__":
-    main()
+    tf_idf = TfIdf(FOLDERS)
+    print(tf_idf.answerQuestion("who killed severus snape"))
+    # tf_idf.metrics(5)
