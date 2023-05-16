bafb8c836aa6ceef88703dfef7e39a13c0e63c47
till4513 <till4513@gmail.com>
2023-05-16 20:53:31 +0200

    update

--- retrieval/preprocessing/process_threaded_question_gen.py
+++ retrieval/preprocessing/process_threaded_question_gen.py
@@ -1,6 +1,6 @@
 preprocessed_json_path = '../../data/fandoms/harry_potter.json'
 question_generated__json_path = 'harry_potter_question_para.csv'
-output_path_dir = "./temp/"
+output_path_dir = "../../data/fandoms_qa/harry_potter/"
 
 if __name__ == "__main__":
 
@@ -101,102 +101,102 @@ if __name__ == "__main__":
     df
 
     # %%
-    df.to_json(output_path_dir + "processed_wiki.json", orient='records', indent=4)
-    df2 = df.copy()
-    df2.columns
+    df.to_json(output_path_dir + preprocessed_json_path.split('/')[-1][:-5] + "_qa"+".json", orient='records', indent=4)
+    # df2 = df.copy()
+    # df2.columns
 
-    # %%
-    queries = []
-    qids = []
-    pids = []
-    pid_wid = []
-    passages = []
-    qid_id = 0
-    pid_id = 0
-    triples = []
-    wikis = []
-    inconsistent = 0
+    # # %%
+    # queries = []
+    # qids = []
+    # pids = []
+    # pid_wid = []
+    # passages = []
+    # qid_id = 0
+    # pid_id = 0
+    # triples = []
+    # wikis = []
+    # inconsistent = 0
 
-    for i in range(len(df2)):
-        row = df.iloc[i]
-        wiki_pids = []
+    # for i in range(len(df2)):
+    #     row = df2.iloc[i]
+    #     wiki_pids = []
 
-        for j in range(len(row['text'])):
-            pids.append(pid_id)
-            passages.append(row['text'][j])
-            pid_wid.append(row['id'])
-            wiki_pids.append(pid_id)
+    #     for j in range(len(row['text'])):
+    #         pids.append(pid_id)
+    #         passages.append(row['text'][j])
+    #         pid_wid.append(row['id'])
+    #         wiki_pids.append(pid_id)
 
-            pid_id = pid_id + 1
-
-
-            if len(row['positive'][j]) != len(row['negative'][j]):
-                inconsistent = inconsistent + 1
-            for k in range(min(len(row['positive'][j]), len(row['negative'][j]))):
-                queries.append(row['positive'][j][k])
-                qids.append(qid_id)
-                qid_id = qid_id + 1
+    #         pid_id = pid_id + 1
 
 
-                queries.append(row['negative'][j][k])
-                qids.append(qid_id)
-                qid_id = qid_id + 1
-                triples.append(( qid_id - 2, qid_id - 1,pid_id - 1))
-        wikis.append(wiki_pids)
-
-    print(inconsistent)
-
-    # %%
-    df2 = df2.drop(['text', 'positive', 'negative'], axis=1)
+    #         if len(row['positive'][j]) != len(row['negative'][j]):
+    #             inconsistent = inconsistent + 1
+    #         for k in range(min(len(row['positive'][j]), len(row['negative'][j]))):
+    #             queries.append(row['positive'][j][k])
+    #             qids.append(qid_id)
+    #             qid_id = qid_id + 1
 
 
-    # %%
-    pid_p_df = pd.DataFrame(zip(pids,passages, pid_wid),columns=['PID', 'passage', 'WID'])
-    qid_q_df = pd.DataFrame(zip(qids,queries),columns=['QID', 'query'])
-    triples_df = pd.DataFrame(triples,columns=['QID+', 'QID-', 'PID'])
-    df2['PIDs'] = wikis
+    #             queries.append(row['negative'][j][k])
+    #             qids.append(qid_id)
+    #             qid_id = qid_id + 1
+    #             triples.append(( qid_id - 2, qid_id - 1,pid_id - 1))
+    #     wikis.append(wiki_pids)
 
-    # %%
-    triples_df.head(3)
+    # print(inconsistent)
 
-    # %%
-    pid_p_df.head(4)
-
-    # %%
-    qid_q_df.head(6)
-
-    # %%
-    triples_df.head(3)
-
-    # %%
-    df2.head(3)
-
-    # %%
-    import json
-    output_path = output_path_dir
-    triples_df.to_csv(output_path+ "triples.tsv",index=False, sep="\t")
-    pid_p_df.to_csv(output_path+ "passages.tsv",index=False, sep="\t")
-    qid_q_df.to_csv(output_path+ "queries.tsv",index=False, sep="\t")
-
-    data = df2.to_dict('index')
-
-    # convert the dictionary to the desired JSON format
-    output = {}
-    for key, value in data.items():
-        output[int(value['id'])] = {
-            'revid': value['revid'],
-            'url': value['url'],
-            'title': value['title'],
-            'PIDs': value['PIDs']
-        }
-
-    # convert the output dictionary to JSON
-    # json_output = json.dumps(output)
-    with open(output_path + 'wiki.json', 'w') as f:
-        json.dump(output, f, indent=4)
+    # # %%
+    # df2 = df2.drop(['text', 'positive', 'negative'], axis=1)
 
 
-    # %%
+    # # %%
+    # pid_p_df = pd.DataFrame(zip(pids,passages, pid_wid),columns=['PID', 'passage', 'WID'])
+    # qid_q_df = pd.DataFrame(zip(qids,queries),columns=['QID', 'query'])
+    # triples_df = pd.DataFrame(triples,columns=['QID+', 'QID-', 'PID'])
+    # df2['PIDs'] = wikis
+
+    # # %%
+    # triples_df.head(3)
+
+    # # %%
+    # pid_p_df.head(4)
+
+    # # %%
+    # qid_q_df.head(6)
+
+    # # %%
+    # triples_df.head(3)
+
+    # # %%
+    # df2.head(3)
+
+    # # %%
+    # import json
+    # output_path = output_path_dir
+    # triples_df.to_csv(output_path+ "triples.tsv",index=False, sep="\t")
+    # pid_p_df.to_csv(output_path+ "passages.tsv",index=False, sep="\t")
+    # qid_q_df.to_csv(output_path+ "queries.tsv",index=False, sep="\t")
+
+    # data = df2.to_dict('index')
+
+    # # convert the dictionary to the desired JSON format
+    # output = {}
+    # for key, value in data.items():
+    #     output[int(value['id'])] = {
+    #         'revid': value['revid'],
+    #         'url': value['url'],
+    #         'title': value['title'],
+    #         'PIDs': value['PIDs']
+    #     }
+
+    # # convert the output dictionary to JSON
+    # # json_output = json.dumps(output)
+    # with open(output_path +  'wiki.json', 'w') as f:
+    #     json.dump(output, f, indent=4)
+
+
+    # # %%
 
 
 
