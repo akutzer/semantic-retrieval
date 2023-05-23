0adef364a7fa4339a4f6a1508a9a7de5d1f9a2bf
till4513 <till4513@gmail.com>
2023-05-23 19:33:39 +0200

    updated process_qa_json to split into train val test

--- retrieval/preprocessing/process_qa_json.py
+++ retrieval/preprocessing/process_qa_json.py
@@ -1,19 +1,29 @@
 import glob
 import os
-
+import tqdm
+import pandas as pd
+import json
 
 preprocessed_qa_json_path_dir = '../../data/fandoms_qa/'
 output_path_dir = "../../data/fandoms_qa/"
 
+# fractions of ['train', 'test', 'val']
+fracs = [0.8, 0.1, 0.1]
+
+# endings of individual files
+# endings = [".train", ".test", ".val"]
+
+# if no file endings for individual files wanted:
+endings = ['']*3
+
+# folder names for each project
+endings_dir = ['train', 'test', 'val']
 
 if __name__ == "__main__":
     for file in os.listdir(preprocessed_qa_json_path_dir):
         if file.endswith(".json"):
+            current_frac = 0
             f = file
-            out_dir = output_path_dir + file.split('/')[-1][:-8] + '/'
-            os.makedirs(out_dir,exist_ok=True)
-
-            import pandas as pd
 
             df2 = pd.read_json(preprocessed_qa_json_path_dir + f, orient ='records')
             df2.columns
@@ -31,8 +41,13 @@ if __name__ == "__main__":
             inconsistent = 0
             exclude = 0
             total = 0
-            import tqdm
+            last_ind = 0
 
+            df2 = df2.sample(frac=1)
+
+            total_p = len([(i,j,k) for i in range(len(df2)) for j in range(len(df2.iloc[i]['text'])) for k in range(min(len(df2.iloc[i]['positive'][j]), len(df2.iloc[i]['negative'][j]))) if not df2.iloc[i]['text'][j].endswith(' .')])
+
+            ii = 0
             for i in tqdm.tqdm(range(len(df2))):
                 row = df2.iloc[i]
                 wiki_pids = []
@@ -57,6 +72,7 @@ if __name__ == "__main__":
 
 
                     for k in range(min(len(row['positive'][j]), len(row['negative'][j]))):
+                        ii = ii + 1
                         # added to not include passages where information was lost due to wikiextractor
                         queries.append(row['positive'][j][k])
                         qids.append(qid_id)
@@ -67,59 +83,67 @@ if __name__ == "__main__":
                         qids.append(qid_id)
                         qid_id = qid_id + 1
                         triples.append(( qid_id - 2, qid_id - 1,pid_id - 1))
+
                 wikis.append(wiki_pids)
+
+                if 1.0*ii/total_p >= sum(fracs[:current_frac + 1]):
+                    if current_frac != 3:
+                        out_dir = output_path_dir + file.split('/')[-1][:-8] + '/' + endings_dir[current_frac] + '/'
+
+                        os.makedirs(out_dir,exist_ok=True)
+                    else:
+                        continue
+
+
+                    pid_p_df = pd.DataFrame(zip(pids,passages, pid_wid), columns=['PID', 'passage', 'WID'])
+                    qid_q_df = pd.DataFrame(zip(qids,queries), columns=['QID', 'query'])
+                    triples_df = pd.DataFrame(triples, columns=['QID+', 'QID-', 'PID'])
+
+                    queries = []
+                    qids = []
+                    pids = []
+                    pid_wid = []
+                    passages = []
+                    qid_id = 0
+                    pid_id = 0
+   
+                    df2n = df2.iloc[last_ind:(i + 1)]
+                    df2n = df2n.drop(['text', 'positive', 'negative'], axis=1)
+
+                    df2n['PIDs'] = wikis
+
+                    last_ind = i + 1
+
+                    output_path = out_dir
+
+                    triples_df.to_csv(output_path + 'triples' + endings[current_frac]+ '.tsv', index=False, sep="\t")
+                    pid_p_df.to_csv(output_path +  'passages' + endings[current_frac] +'.tsv', index=False, sep="\t")
+                    qid_q_df.to_csv(output_path + 'queries' + endings[current_frac] +'.tsv', index=False, sep="\t")
+
+
+
+                    data = df2n.to_dict('index')
+
+                    # convert the dictionary to the desired JSON format
+                    output = {}
+                    for key, value in data.items():
+                        output[int(value['id'])] = {
+                            'revid': value['revid'],
+                            'url': value['url'],
+                            'title': value['title'],
+                            'PIDs': value['PIDs']
+                        }
+
+
+                    # convert the output dictionary to JSON
+                    # json_output = json.dumps(output)
+                    with open(output_path + 'wiki'+ endings[current_frac] +'.json', 'w') as f:
+                        json.dump(output, f, indent=4)
+                    
+                    wikis = []
+                    
+                    current_frac = current_frac + 1
+                
 
             print('inconsistent: ' + str(inconsistent))
             print('excluded: ' + str(1.0*exclude/total))
-
-            # %%
-            df2 = df2.drop(['text', 'positive', 'negative'], axis=1)
-
-
-            # %%
-            pid_p_df = pd.DataFrame(zip(pids,passages, pid_wid), columns=['PID', 'passage', 'WID'])
-            qid_q_df = pd.DataFrame(zip(qids,queries), columns=['QID', 'query'])
-            triples_df = pd.DataFrame(triples, columns=['QID+', 'QID-', 'PID'])
-            df2['PIDs'] = wikis
-
-            # %%
-            triples_df.head(3)
-
-            # %%
-            pid_p_df.head(4)
-
-            # %%
-            qid_q_df.head(6)
-
-            # %%
-            triples_df.head(3)
-
-            # %%
-            df2.head(3)
-
-            # %%
-            import json
-            output_path = out_dir
-            triples_df.to_csv(output_path + "triples.tsv", index=False, sep="\t")
-            pid_p_df.to_csv(output_path + "passages.tsv", index=False, sep="\t")
-            qid_q_df.to_csv(output_path + "queries.tsv", index=False, sep="\t")
-
-            data = df2.to_dict('index')
-
-            # convert the dictionary to the desired JSON format
-            output = {}
-            for key, value in data.items():
-                output[int(value['id'])] = {
-                    'revid': value['revid'],
-                    'url': value['url'],
-                    'title': value['title'],
-                    'PIDs': value['PIDs']
-                }
-
-            # convert the output dictionary to JSON
-            # json_output = json.dumps(output)
-            with open(output_path + 'wiki.json', 'w') as f:
-                json.dump(output, f, indent=4)
-
-
-            # %%
