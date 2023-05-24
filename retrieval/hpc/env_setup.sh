bf16d3fa02d5a7f8c851557a9d2b3d9ac3823b1d
Tommy Nguyen <thinktommy@gmx.de>
2023-05-24 12:48:50 +0200

    clean format

--- retrieval/hpc/env_setup.sh
+++ retrieval/hpc/env_setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --time=0:10:00   # walltime
-#SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks=1	# limit to one node
-#SBATCH --cpus-per-task=1  # number of processor cores (i.e. threads)
+#SBATCH --time=0:10:00          # walltime
+#SBATCH --nodes=1               # number of nodes
+#SBATCH --ntasks=1	            # limit to one node
+#SBATCH --cpus-per-task=1       # number of processor cores (i.e. threads)
 #SBATCH --partition=haswell
-#SBATCH --mem-per-cpu=8000M   # memory per CPU core
-#SBATCH -A p_sp_bigdata # name of the associated project
-#SBATCH -J "env_setup_job" # name of the job
-#SBATCH -o "env_setup_job"-%j.out" # output file name (std out)
-#SBATCH --error="env_setup_job"-%j.err" # error file name (std err)
+#SBATCH --mem-per-cpu=8000M     # memory per CPU core
+#SBATCH -A p_sp_bigdata         # name of the associated project
+#SBATCH -J "env_setup_job"      # name of the job
+#SBATCH --output="env_setup_job"-%j.out"    # output file name (std out)
+#SBATCH --error="env_setup_job"-%j.err"     # error file name (std err)
 #SBATCH --mail-user="tommy.nguyen@mailbox.tu-dresden.de" # will be used to used to update you about the state of your$
 #SBATCH --mail-type ALL
 
-# HPC-Cluster doesn't have new Python Version 
+# HPC-Cluster doesn't have newer Python Version 
 module load Python/3.10.4
 
 # switch to virtualenv to setup our environment, modules we need 
