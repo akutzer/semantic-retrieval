0fc415d77444b266cc1fd8e875de378f84439e1a
tong623c <tong623c@tauruslogin6.taurus.hrsk.tu-dresden.de>
2023-05-26 17:48:18 +0200

    fix typos and lower Mem Usage

--- retrieval/hpc/simple_training.sh
+++ retrieval/hpc/simple_training.sh
@@ -2,14 +2,14 @@
 #SBATCH --time=04:00:00         # walltime
 #SBATCH --nodes=1               # number of nodes
 #SBATCH --ntasks=1	            # limit to one node
-#SBATCH --cpu-per-task=1
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu2
 #SBATCH --gres=gpu:1            # number of GPUs
-#SBATCH --mem=80G
+#SBATCH --mem=40G
 #SBATCH -A p_sp_bigdata         # name of the associated project
-#SBATCH -J "test_ms_marco_job"  # name of the job
-#SBATCH --output="test_ms-marco_job-%j.out"    # output file name (std out)
-#SBATCH --error="test_ms-marco_job-%j.err"     # error file name (std err)
+#SBATCH -J "simple_training_job"  # name of the job
+#SBATCH --output="simple_training_job-%j.out"    # output file name (std out)
+#SBATCH --error="simple_training_job-%j.err"     # error file name (std err)
 #SBATCH --mail-user="tommy.nguyen@mailbox.tu-dresden.de" # will be used to used to update you about the state of your$
 #SBATCH --mail-type ALL
 
