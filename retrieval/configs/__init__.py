47ee0f478a9cb669cd3b746406c66c926100eec8
Aaron Kutzer <aaron.kutzer@mailbox.tu-dresden.de>
2023-05-27 14:01:13 +0200

    Added training script for MS MARCO

--- retrieval/configs/__init__.py
+++ retrieval/configs/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
-from .configs import BaseConfig, FANDOM_CONFIG_DEFAULT
+from .configs import BaseConfig, FANDOM_DEFAULT_CONFIG, MS_MARCO_DEFAULT_CONFIG
 from .utils import save_config, load_config
