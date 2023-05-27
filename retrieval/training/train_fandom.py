4f7b9634822c3aedd49336749b5487d7c01a01bd
Aaron Kutzer <aaron.kutzer@mailbox.tu-dresden.de>
2023-05-27 13:46:22 +0200

    Added MRR as a metric to the script

--- retrieval/training/train_fandom.py
+++ retrieval/training/train_fandom.py
@@ -12,8 +12,9 @@ from retrieval.training.utils import seed, get_tensorboard_writer, get_run_name
 def validation(model, criterion, dataloader, config):
     model.eval()
     with torch.inference_mode():
-        eval_loss = 0.0
-        eval_acc = 0.0
+        loss = 0.0
+        acc = 0.0
+        mrr = 0.0
 
         for Q, P in dataloader:
             # forward pass through the model & calculate loss
@@ -21,14 +22,18 @@ def validation(model, criterion, dataloader, config):
                 out = model(Q, P)
                 # target is the 0-th aka first question from the list of queries given to a passage
                 target = torch.zeros(out.shape[0], device=out.device, dtype=torch.long)
-                eval_loss += criterion(out, target)
-                eval_acc += torch.sum(out.max(dim=-1).indices == target)
+                loss += criterion(out, target)
+                acc += torch.sum(out.max(dim=-1).indices == target)
+                ranks = out.sort(dim=-1, descending=True).indices
+                positiv_rank = torch.where(ranks == 0)[-1]
+                mrr += torch.sum(1.0 / (positiv_rank + 1).float())
         
-        eval_loss /= len(eval_dataset)
-        eval_acc /= len(eval_dataset)
+        loss /= len(eval_dataset)
+        acc /= len(eval_dataset)
+        mrr /= len(eval_dataset)
 
     model.train()
-    return eval_loss, eval_acc
+    return loss, acc, mrr
 
 
 if __name__ == "__main__":
@@ -47,7 +52,9 @@ if __name__ == "__main__":
     DATASET_PATH = "../../data/fandoms_qa/harry_potter/"
     RUN_NAME = get_run_name(config, DATASET_NAME)
 
-    N_EVAL_PER_EPOCH = 5
+    N_EVAL_PER_EPOCH = 6
+    N_CHECKPOINTS_PER_EPOCH = 2
+    assert N_EVAL_PER_EPOCH % N_CHECKPOINTS_PER_EPOCH == 0
 
     # instantiation of GradScaler for automatic mixed precission
     if config.use_amp:
@@ -86,25 +93,29 @@ if __name__ == "__main__":
 
     print("[LOGGING] Starting initial evaluation!")
     # evaluate the model on the entire validation set after each epoch
-    eval_loss, eval_acc = validation(colbert, criterion, eval_dataloader, config)
-    print(eval_loss.item(), eval_acc.item())
+    eval_loss, eval_acc, eval_mrr = validation(colbert, criterion, eval_dataloader, config)
+    print(eval_loss.item(), eval_acc.item(), eval_mrr.item())
     
     # write the metric to the tensorboard logger
     writer.add_scalar("Loss/eval", eval_loss, 0)
     writer.add_scalar("Accuracy/eval", eval_acc, 0)
+    writer.add_scalar("MRR/eval", eval_mrr, 0)
 
 
     print("[LOGGING] Starting training!")
     for epoch in range(1, config.epochs+1):
         print(f"[LOGGING] Starting epoch: {epoch}")
         optimizer.zero_grad()
-        losses, accs = 0, 0
+        losses, accs, mrr = 0.0, 0.0, 0.0
 
-        # calculate after which steps we need to run the evaluation
+        # calculate after which steps we need to run the evaluation & save a checkpoint
         # and make sure to run the last evaluation after the last iteration of
         # the training dataloader
         eval_iterations = [i * (len(train_dataloader) // N_EVAL_PER_EPOCH) for i in range(1, N_EVAL_PER_EPOCH + 1)]
         eval_iterations[-1] = len(train_dataloader)
+
+        checkpoint_iterations = [i * (len(train_dataloader) // N_CHECKPOINTS_PER_EPOCH) for i in range(1, N_CHECKPOINTS_PER_EPOCH + 1)]
+        checkpoint_iterations[-1] = len(train_dataloader)
 
         colbert.train()
         for i, (Q, P) in enumerate(tqdm(train_dataloader)):
@@ -131,6 +142,9 @@ if __name__ == "__main__":
                 losses += loss.item()
                 # calculate the accuracy within a subbatch -> extremly inflated accuracy
                 accs += torch.sum(out.detach().max(dim=-1).indices == torch.zeros(sub_B, device=out.device, dtype=torch.long))
+                ranks = out.sort(dim=-1, descending=True).indices
+                positiv_rank = torch.where(ranks == 0)[-1]
+                mrr += torch.sum(1.0 / (positiv_rank + 1).float())
             
             # after accum_steps, update the weights + log the metrics
             if (i + 1) % config.accum_steps == 0:
@@ -146,24 +160,27 @@ if __name__ == "__main__":
                 time_step = (epoch - 1) * (len(train_dataloader) // config.accum_steps)  + i // config.accum_steps
                 writer.add_scalar("Loss/train", losses, time_step)
                 writer.add_scalar("Accuracy/train", accs / config.batch_size, time_step)
-                losses, accs = 0, 0
+                writer.add_scalar("MRR/train", mrr / config.batch_size, time_step)
+                losses, accs, mrr = 0.0, 0.0, 0.0
             
 
             if (i + 1) in eval_iterations:
                 # evaluate the model on the entire validation set after each epoch
-                eval_loss, eval_acc = validation(colbert, criterion, eval_dataloader, config)
-                print(eval_loss.item(), eval_acc.item())
+                eval_loss, eval_acc, eval_mrr = validation(colbert, criterion, eval_dataloader, config)
+                print(eval_loss.item(), eval_acc.item(), eval_mrr.item())
                 
                 # write the metric to the tensorboard logger
                 time_step = (epoch - 1) * (len(train_dataloader) // config.accum_steps)  + i // config.accum_steps
                 writer.add_scalar("Loss/eval", eval_loss, time_step)
                 writer.add_scalar("Accuracy/eval", eval_acc, time_step)
-
-
-        # shitty checkpointing
-        checkpoint_path = f"../../checkpoint/{RUN_NAME}/epoch{epoch}_loss%.5f_acc%.3f" % (eval_loss.item(), eval_acc.item() * 100)
-        colbert.save(checkpoint_path)
-        tokenizer.save(checkpoint_path, store_config=False)
+                writer.add_scalar("MRR/eval", eval_mrr, time_step)
+            
+            if (i + 1) in checkpoint_iterations:
+                # shitty checkpointing
+                checkpoint_in_epoch = checkpoint_iterations.index(i + 1) + 1
+                checkpoint_path = f"../../checkpoint/{RUN_NAME}/epoch{epoch}_{checkpoint_in_epoch}_loss%.4f_mrr%.4f_acc%.3f" % (eval_loss.item(), eval_mrr.item(), eval_acc.item() * 100)
+                colbert.save(checkpoint_path)
+                tokenizer.save(checkpoint_path, store_config=False)
     
 
     print("[LOGGING] Training finished")
