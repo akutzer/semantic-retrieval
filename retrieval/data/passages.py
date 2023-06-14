2ebd28046670dde10a09766f4a384619f106433e
Aaron Kutzer <aaron.kutzer@mailbox.tu-dresden.de>
2023-06-14 08:42:14 +0200

    Updated passages.py to keep the WIDs

--- retrieval/data/passages.py
+++ retrieval/data/passages.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 # TODO: Add WID support
 class Passages:
-    def __init__(self, path=None):
+    def __init__(self, path=None, ignore_wid=True):
         self.path = path
+        self.ignore_wid = ignore_wid
         self.data = pd.Series(dtype="object")
         self._load_file(path)
 
@@ -14,23 +15,28 @@ class Passages:
         return len(self.data)
 
     def __iter__(self):
-        return iter(self.data.items())
+        data = self.data.iloc[:, 0] if self.ignore_wid else self.data
+        return iter(data.items())
 
     def __getitem__(self, key):
-        return self.data.loc[key]
+        data = self.data.iloc[:, 0] if self.ignore_wid else self.data
+        return data.loc[key]
 
     def keys(self):
-        return self.data.index
+        data = self.data.iloc[:, 0] if self.ignore_wid else self.data
+        return data.index
 
     def values(self):
-        return self.data.values
+        data = self.data.iloc[:, 0] if self.ignore_wid else self.data
+        return data.values
 
     def items(self):
-        return self.data.items()
+        data = self.data.iloc[:, 0] if self.ignore_wid else self.data
+        return data.items()
 
     def _load_file(self, path):
         if path.endswith((".csv", ".tsv")):
-            self.data = self._load_tsv(path)
+            self.data = self._load_tsv(path, drop_nan=True)
 
         elif path.endswith(".json"):
             raise DeprecationWarning()
@@ -39,16 +45,14 @@ class Passages:
         return self.data
 
     def _load_tsv(self, path, drop_nan=False):
-        logging.basicConfig(level=logging.WARNING, format="[%(asctime)s][%(levelname)s] %(message)s", datefmt="%y-%m-%d %H:%M:%S")
-        logging.warning("Passages currently drops the WID column!")
+        # logging.basicConfig(level=logging.WARNING, format="[%(asctime)s][%(levelname)s] %(message)s", datefmt="%y-%m-%d %H:%M:%S")
+        # logging.warning("Passages currently drops the WID column!")
         delimiter = "\t" if path.endswith(".tsv") else ","
         passages = pd.read_csv(path, delimiter=delimiter, index_col=False)
 
         self._replace_nan(passages, drop_nan)
-        pid, passage, *_ = passages.columns
-        # convert the pandas.DataFrame with the columns QID and query
-        # into a pandas.Series
-        passages = passages.set_index(pid, drop=False)[passage]
+        pid, passage, wid, *_ = passages.columns
+        passages = passages.set_index(pid, drop=False)[[passage, wid]]
         self._rename_axis(passages)
         return passages
 
@@ -92,7 +96,8 @@ if __name__ == "__main__":
     path = "../../data/fandoms_qa/harry_potter/train/passages.tsv"
     passages = Passages(path=path)
     print(passages.data, end="\n\n")
-
+    
     print(len(passages))
-    print(passages[0])
+    print(passages.values())
+    print(passages[173654], type(passages[173654]))
     print(passages.pid2string([0, 2 * len(passages)], skip_non_existing=True))
