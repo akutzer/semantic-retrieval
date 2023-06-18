1a6ce554fa75993b90deb32f61ec8f9f54c7fa97
zhzh622c <zhzh622c@tauruslogin6.taurus.hrsk.tu-dresden.de>
2023-06-18 04:58:42 +0200

    updata .sh files to run on hpc

--- retrieval/evaluation/colbert_indexer_fandom.sh
+++ retrieval/evaluation/colbert_indexer_fandom.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
+BATCH --time=00:15:00         # walltime
+#SBATCH --nodes=1               # number of nodes
+#SBATCH --ntasks=1	            # limit to one node
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=alpha
+#SBATCH --gres=gpu:1            # number of GPUs
+#SBATCH --mem=32G
+#SBATCH -A p_sp_bigdata         # name of the associated project
+#SBATCH -J "fandom_harry_potter"  # name of the job
+#SBATCH --output="indexer_fandom_job-%j.out"    # output file name (std out)
+#SBATCH --error="indexer_fandom_job-%j.err"     # error file name (std err)
+#SBATCH --mail-user="zhiwei.zhan@mailbox.tu-dresden.de" # will be used to used to update you about the state of your$
+#SBATCH --mail-type ALL
+
+# clean current modules
+module purge
+
+# HPC-Cluster doesn't have newer Python Version 
+module load Python/3.10.4
+
+# switch to virtualenv to setup our environment, modules we need 
+# if needed change to your own workspace
+virtualenv --system-site-packages /scratch/ws/0/zhzh622c-test-workspace/env
+source /scratch/ws/0/zhzh622c-test-workspace/env/bin/activate
 
 # Set the arguments for the Python script:
 # dataset arguments
@@ -16,3 +39,5 @@ python colbert_indexer.py \
     --indexer  "$INDEXER"\
     --checkpoint "$CHECKPOINT" \
     --passages_path_val "$PASSAGES_PATH_VAL" \
+
+deactivate
