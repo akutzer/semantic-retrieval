0d32696675caee213d07652e4af9a1e7c722cd5f
FloFmm <florian_maurus.mueller@mailbox.tu-dresden.de>
2023-06-26 12:15:53 +0200

    testing

--- retrieval/indexing/index.sh
+++ retrieval/indexing/index.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Set the arguments for the Python script:
-PASSAGES_PATH="../../scratch/ws/0/tong623c-tommy-workspace/semantic-retrieval/data/fandoms_qa/witcher/all/passages.tsv"
-INDEX_PATH="../../scratch/ws/0/tong623c-tommy-workspace/semantic-retrieval/data/fandoms_qa/witcher/all/passages_all.index.pt"
+#PASSAGES_PATH="../../scratch/ws/0/tong623c-tommy-workspace/semantic-retrieval/data/fandoms_qa/witcher/all/passages.tsv"
+PASSAGES_PATH = "../../data/fandoms_qa/fandoms_all/human_verified/final/witc/all/passages_test.tsv"
+INDEX_PATH = "../../data/fandoms_qa/fandoms_all/human_verified/final/witc/all/passages_test.index.pt"
+#INDEX_PATH="../../scratch/ws/0/tong623c-tommy-workspace/semantic-retrieval/data/fandoms_qa/witcher/all/passages_all.index.pt"
 # PASSAGES_PATH="../../data/fandoms_qa/fandoms_all/val/passages.tsv"
 # INDEX_PATH="../../data/fandoms_qa/fandoms_all/val/passages.index.pt"
 CHECKPOINT_PATH="../../data/colbertv2.0"
-DTYPE="FP16"  # FP16, FP32, FP64
+DTYPE="FP32"  # FP16, FP32, FP64
 BATCH_SIZE="8"
 
 
